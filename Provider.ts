56b5072f00c9471e27d74baca56416f1061af545
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-10-10 17:41:39 +0300

    Fix types for `provide` and `resolve`

--- Provider.ts
+++ Provider.ts
@@ -1,8 +1,10 @@
 import { serviceMapSingleton } from "./ServiceStorage.ts";
 
 export function provide<
-  C extends new (...args: Array<unknown>) => unknown,
-  D extends Array<new (...args: Array<unknown>) => unknown>
+  // deno-lint-ignore no-explicit-any
+  C extends new (...args: Array<any>) => unknown,
+  // deno-lint-ignore no-explicit-any
+  D extends Array<new (...args: Array<any>) => unknown>
 >(clazz: C, deps: D): void {
   return serviceMapSingleton.provides(clazz, deps);
 }
--- Resolver.ts
+++ Resolver.ts
@@ -1,6 +1,7 @@
 import { serviceMapSingleton } from "./ServiceStorage.ts";
 
-export function resolve<C extends new (...args: Array<unknown>) => unknown>(
+// deno-lint-ignore no-explicit-any
+export function resolve<C extends new (...args: Array<any>) => unknown>(
   clazz: C
 ): InstanceType<C> {
   return serviceMapSingleton.resolve(clazz);
